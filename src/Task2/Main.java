62ee98e95b8936f505d6b5ca2241359b2f3ab3fc
UladzimirIvanov <ivanouum@gmail.com>
2024-04-17 17:55:17 +0300

    Task2 release

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -13,5 +13,27 @@ public class Main {
         Circle circle = new Circle(12);
         System.out.println("Площать круга равна: " + circle.square());
         System.out.println("Периметр круга равен: " + circle.perimeter());
+
+        System.out.println();
+        System.out.println("*********************************");
+        System.out.println();
+
+        Triangle triangle = new Triangle(4, 7);
+        System.out.println("Площать реугольника равна: " + triangle.square());
+        System.out.println("Периметр треугольника равен: " + triangle.perimeter());
+
+        System.out.println();
+        System.out.println("*********************************");
+        System.out.println();
+
+        double a = 0;
+        double[] arr = {triangle.perimeter(), circle.perimeter(), rectangle.perimeter(), circle.perimeter(), triangle.perimeter()};
+        for (int i = 0; i < arr.length; i++) {
+            a = a + arr[i];
+        }
+        System.out.println(a);
+
+
+        /*Мне кажется, что в этой задаче родитель Figure максимально бесполезен, я всё равно расписывал методы в каждом наследнике*/
     }
 }
--- src/Task2/Triangle.java
+++ src/Task2/Triangle.java
@@ -1,4 +1,28 @@
 package Task2;
 
-public class Triangle {
+public class Triangle extends Figure {
+    /*Площадь треугольника вычисляется в зависимости от самого треугольника и от известных нам данных.
+     * Но я возьму в расчёт, что треугольник мой имеет один прямой угол и известны две стороны.*/
+
+    double a;
+    double b;
+    double s;
+    double p;
+
+    @Override
+    double square() {
+        s = (a * b) / 2;
+        return s;
+    }
+
+    @Override
+    double perimeter() {
+        p = Math.sqrt((a * a) + (b * b)) + a + b;
+        return p;
+    }
+
+    public Triangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
 }
